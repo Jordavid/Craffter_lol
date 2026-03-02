a88efb33f409398dd5961070bf69606e33cf8783
Jordavid <jordavid.perez@gmail.com>
2026-03-01 20:38:20 -0600

    fix: allow wildcard CORS origins with allowedOriginPatterns

--- Back/crafter/src/main/java/com/crafter/league/of/legends/crafter/config/CorsConfig.java
+++ Back/crafter/src/main/java/com/crafter/league/of/legends/crafter/config/CorsConfig.java
@@ -21,25 +21,11 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(allowedOrigins)
+                .allowedOriginPatterns(allowedOrigins)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
                 .maxAge(3600);
     }
 
-    /*@Bean
-    public CorsFilter corsFilter() {
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.setAllowedOrigins(Arrays.asList(allowedOrigins));
-        config.setAllowedHeaders(List.of("*"));
-        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        config.setMaxAge(3600L);
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config);
-
-        return new CorsFilter(source);
-    }*/
 }
